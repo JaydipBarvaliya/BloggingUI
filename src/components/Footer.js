e5f213f02e0733abdeb17e31955f297365304cdd
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-01-25 03:14:05 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-100 dark:bg-gray-900 py-6 text-center">
       <p className="text-gray-600 dark:text-gray-400">
-        &copy; {new Date().getFullYear()} Blogging Platform. All rights reserved.
+        &copy; {new Date().getFullYear()} Data Engineering Platform. All rights reserved.
       </p>
     </footer>
 

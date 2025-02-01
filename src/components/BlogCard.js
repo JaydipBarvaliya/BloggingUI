5220d649f62a93dfa9c239a2fee3de32bddbd34f
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-02-01 01:55:12 -0500

    Added zooming and shadow effect on hover

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -8,12 +8,12 @@ const BlogCard = ({ blog, isFavorite, onToggleFavorite }) => {
   const defaultImage = "https://picsum.photos/600/400";
 
   return (
-    <div className="bg-white dark:bg-gray-800 shadow-lg hover:shadow-xl transition-shadow duration-200 rounded-lg overflow-hidden">
+    <div className="bg-white dark:bg-gray-800 shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300 rounded-lg overflow-hidden">
       {/* Blog Image */}
       <img
         src={blog.image || defaultImage}
         alt={blog.title || "Blog Image"}
-        className="w-full h-48 object-cover rounded-t-lg"
+        className="w-full h-48 object-cover rounded-t-lg transition-transform duration-300 transform hover:scale-105"
         loading="lazy"
       />
 
@@ -32,7 +32,7 @@ const BlogCard = ({ blog, isFavorite, onToggleFavorite }) => {
         {/* Actions */}
         <div className="flex justify-between items-center mt-4">
           {/* Read More Button */}
-          <Link to={`/blogs/${blog.id}`}>
+          <Link to={`/blogs/${blog.id}`} aria-label={`Read more about ${blog.title}`}>
             <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 dark:bg-blue-600 dark:hover:bg-blue-700 transition duration-200">
               Read More
             </button>

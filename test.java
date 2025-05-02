a1e4378d9f6ef8b2c74652fe71239964639d930e
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-05-02 15:45:54 -0400

    Update test.java

--- test.java
+++ test.java
@@ -10,3 +10,16 @@ private UpdateSignerRequest buildRequestWithInvalidType(String invalidType, Stri
     request.setSigner(List.of(signer));
     return request;
 }
+
+
+
+@Test
+void testDefaultInvalidAuthType_throwsException() {
+    UpdateSignerRequest request = buildRequestWithInvalidType("INVALID_TYPE", "1234567890");
+
+    SharedServiceLayerException ex = assertThrows(SharedServiceLayerException.class, () ->
+        validateAuthTypeUtil.validateAuthType(request, "lob")
+    );
+
+    assertEquals("Authentication.authenticationMethodTypeCd is not a valid type", ex.getMessage());
+}

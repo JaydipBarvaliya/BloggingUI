8a79a361a9e42763484c74c7c0882c10f40d591f
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-05-05 13:23:14 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,16 +1,26 @@
-public boolean isEmailAssociatedWithSigner(SharedServicePackage sharedServicePackage, String email) throws SharedServiceLayerException {
+public boolean isEmailAssociatedWithSigner(SharedServicePackage sharedServicePackage, String email)
+        throws SharedServiceLayerException {
+
     if (CollectionUtils.isEmpty(sharedServicePackage.getRoles())) return false;
 
-    return sharedServicePackage.getRoles().stream()
-            .filter(role -> CollectionUtils.isNotEmpty(role.getSigners()))
-            .flatMap(role -> role.getSigners().stream()
-                    .filter(signer -> signer != null && StringUtils.equalsIgnoreCase(signer.getEmail(), email))
-                    .map(signer -> {
-                        RoleType type = role.getType();
-                        if (type == RoleType.SENDER) {
-                            throw CommonUtil.buildBadRequestException("Provided email address belongs to a SENDER, not a SIGNER.");
-                        }
-                        return type == RoleType.SIGNER;
-                    }))
-            .anyMatch(Boolean::booleanValue);
+    try {
+        return sharedServicePackage.getRoles().stream()
+                .filter(role -> CollectionUtils.isNotEmpty(role.getSigners()))
+                .flatMap(role -> role.getSigners().stream()
+                        .filter(signer -> signer != null && StringUtils.equalsIgnoreCase(signer.getEmail(), email))
+                        .map(signer -> {
+                            RoleType type = role.getType();
+                            if (type == RoleType.SENDER) {
+                                throw new RuntimeException(CommonUtil.buildBadRequestException(
+                                        "Provided email address belongs to a SENDER, not a SIGNER."));
+                            }
+                            return type == RoleType.SIGNER;
+                        }))
+                .anyMatch(Boolean::booleanValue);
+    } catch (RuntimeException ex) {
+        if (ex.getCause() instanceof SharedServiceLayerException) {
+            throw (SharedServiceLayerException) ex.getCause();
+        }
+        throw ex;
+    }
 }

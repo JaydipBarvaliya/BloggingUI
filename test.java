c0d03320deb87a0bddd6aedee28bc7812e684dcf
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-05-08 16:28:07 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,9 @@
 if (!isSafePathComponent(baseFolder) || !isSafePathComponent(subFolder)) {
     throw new IllegalArgumentException("Invalid folder name.");
 }
+
+
+
+public static boolean isSafePathComponent(String input) {
+    return input != null && input.matches("^[\\w\\-]+$"); // only letters, numbers, underscore, dash
+}

c87f186c52a62f79ab18a9fcc16090867117cdcd
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-05-08 16:46:32 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,2 @@
-public static boolean isSafePathComponent(String input) {
-    return input != null 
-        && input.matches("^[\\w.-]+$")     // alphanumeric, underscore, dash, dot
-        && !input.contains("..")           // prevent directory traversal
-        && !input.contains("/") 
-        && !input.contains("\\");
-}
+Path imagePath = Paths.get(path, packageId + ".png").normalize();
+File f = imagePath.toFile();

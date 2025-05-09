1be66a33b7b2da01ff9f9600c8c23b128439c1b4
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-05-09 14:03:32 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,4 +1,3 @@
-curl -v -X DELETE "https://localhost:8443/esignatureevents/$EVENT_ID/documentpackage" \
+curl -k -X DELETE "https://localhost:8443/esignatureevents/t7wHS6rf3F5Ve4F2mOYmFWV0hrY=/documentpackage" \
   -H "Content-Type: application/json" \
-  -d '{"documents":["doc1","doc2"]}' \
-  -k
+  -d '{"documents":["01e3e197f2d54b2797cc40787976e1b135353b920c0b76c5","8407dc403dabb4e120a95ddfb385930a57c2586490781c26"]}'

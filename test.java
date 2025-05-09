f45e001b84b2c7d7e495f444b20b8cbe7e559519
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-05-09 13:22:03 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,21 @@
-Path safeBase = Paths.get(baseFolder).normalize();
-Path fullPath = safeBase.resolve(Paths.get(subFolder)).normalize();
-if (!fullPath.startsWith(safeBase)) {
-    throw new IllegalArgumentException("Unsafe folder path traversal");
+public static String createDirectories(String baseFolder, String... subFolder) {
+    log.info("createDirectories");
+
+    if (isUnsafePathComponent(baseFolder) || Arrays.stream(subFolder).anyMatch(SignatureTransformUtil::isUnsafePathComponent)) {
+        throw new IllegalArgumentException("Invalid folder name.");
+    }
+
+    Path path = Paths.get(baseFolder, subFolder).normalize();
+    Path safeBase = Paths.get(baseFolder).normalize();
+
+    if (!path.startsWith(safeBase)) {
+        throw new IllegalArgumentException("Unsafe folder path traversal");
+    }
+
+    try {
+        path = Files.createDirectories(path);
+    } catch (IOException e) {
+        log.error("Failed to create Directory:");
+    }
+    return path.toString();
 }

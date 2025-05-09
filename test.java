3544f90400a071f5f7a09697d73c4abfaec8bb41
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-05-09 12:11:51 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,4 +1,5 @@
 if (isUnsafePathComponent(packageId)) {
     throw new IllegalArgumentException("Invalid package ID.");
 }
-Path path = Paths.get(directoryPath, packageId + ".png").normalize();
+Path deletePath = Paths.get(directoryPath, packageId).normalize();
+FileUtils.forceDelete(deletePath.toFile());

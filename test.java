97020d2c8ad31ab32c9891b677f4fc381f0a5b4c
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-05-16 12:40:34 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,13 +1,31 @@
-import org.junit.Test;
+package com.td.esig.api.util;
 
-public class BesigopsapiUtilTest {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 
-    @Test
-    public void requestBodyNullCheck_ShouldPass_WhenObjectIsNotNull() throws SharedServiceLayerException {
-        // Pass any non-null object
-        Object validObj = new Object();
+public class HttpClientExceptionParser {
 
-        // This should not throw any exception
-        BesigopsapiUtil.requestBodyNullCheck(validObj);
+    public static RuntimeException parse(HttpClientErrorException httpEx) {
+        try {
+            String jsonBody = httpEx.getResponseBodyAsString();
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode root = objectMapper.readTree(jsonBody);
+
+            if (httpEx.getStatusCode() == HttpStatus.NOT_FOUND) {
+                String documentId = root.path("parameters").path("documentIds").asText("unknown");
+                String message = String.format("The following Document Id's does not exist: %s", documentId);
+                return CommonUtil.buildBadRequestException(message);
+
+            } else if (httpEx.getStatusCode() == HttpStatus.BAD_REQUEST) {
+                String message = root.path("message").asText("unknown");
+                return CommonUtil.buildBadRequestException(message);
+            }
+        } catch (Exception ex) {
+            return CommonUtil.buildBadRequestException("Failed to parse error response: " + ex.getMessage());
+        }
+
+        return CommonUtil.buildBadRequestException("Unhandled error occurred while processing response");
     }
 }
